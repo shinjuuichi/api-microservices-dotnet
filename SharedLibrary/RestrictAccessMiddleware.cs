7000e6c3f3c8fb83cbb4d438c70972f24c5c55d3
SinlTB <sinlthai570@gmail.com>
2025-01-31 01:59:52 +0700

    Refactor project references and add SharedLibrary project with RestrictAccessMiddleware

---
+++ SharedLibrary/RestrictAccessMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace SharedLibrary
+{
+    public class RestrictAccessMiddleware(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var referrer = context.Request.Headers["Referer"].FirstOrDefault();
+            if(string.IsNullOrEmpty(referrer))
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("Cant reach.");
+                return;
+            }
+            else
+            {
+                await next(context);
+            }
+        }
+    }
+}
